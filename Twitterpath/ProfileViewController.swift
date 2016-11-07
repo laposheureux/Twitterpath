9888f0b027fee95bde699dec90efa28175b81d26
Aaron L'Heureux <aaron@dotstrosity.net>
2016-11-06 23:39:08 -0800

    Storyboard and view controller setup

---
+++ Twitterpath/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  Twitterpath
+//
+//  Created by Aaron on 11/6/16.
+//  Copyright © 2016 Aaron L'Heureux. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
